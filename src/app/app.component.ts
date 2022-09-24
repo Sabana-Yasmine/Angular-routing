ceec2e288cfcc177653d90dcdac3a5345325832d
Sabana-Yasmine <sabana.platosys@gmail.com>
2022-09-24 18:44:34 +0530

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'routing';
+
+  isLogin : any = "false";
+
+  ngOnInit(){
+    this.isLogin = localStorage.getItem('login')
+  }
 }
